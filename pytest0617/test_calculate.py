a192cbad252f0e15466aebaf2a364de6e7ed9fbd
user <275915667@qq.com>
2021-06-18 16:25:21 +0800

    添加

--- pytest0617/test_calculate.py
+++ pytest0617/test_calculate.py
@@ -1,4 +1,3 @@
-# -*- coding:utf-8 -*-
 import pytest
 from pytest0617.calculate import Calculate
 from util.read_data import ReadData
@@ -18,7 +17,8 @@ class TestCalculate:
         print("计算结束")
 
     @pytest.mark.add
-    @pytest.mark.parametrize(['a', 'b', 'expect'], data.get("add_success"))
+    @pytest.mark.parametrize(data.get("args"), data.get("add_success").get("data"),
+                             ids=data.get("add_success").get("ids"))
     def test_add_success(self, a, b, expect):
         if isinstance(a, float) or isinstance(b, float):
             a = Decimal(str(a))
@@ -28,12 +28,12 @@ class TestCalculate:
 
     @pytest.mark.add
     @pytest.mark.xfail
-    @pytest.mark.parametrize(['a', 'b', 'expect'], data.get("add_fail"))
+    @pytest.mark.parametrize(data.get("args"), data.get("add_fail").get("data"), ids=data.get("add_fail").get("ids"))
     def test_add_fail(self, a, b, expect):
         assert self.cal.add(a, b) == expect
 
     @pytest.mark.div
-    @pytest.mark.parametrize(['a', 'b', 'expect'], data.get("div_success"))
+    @pytest.mark.parametrize(data.get("args"), data.get("div_success").get("data"), ids=data.get("div_success").get("ids"))
     def test_div_success(self, a, b, expect):
         if isinstance(a, float) or isinstance(b, float):
             a = Decimal(str(a))
@@ -43,6 +43,6 @@ class TestCalculate:
 
     @pytest.mark.div
     @pytest.mark.xfail
-    @pytest.mark.parametrize(['a', 'b', 'expect'], data.get("div_fail"))
+    @pytest.mark.parametrize(data.get("args"), data.get("div_fail").get("data"), ids=data.get("div_fail").get("ids"))
     def test_div_fail(self, a, b, expect):
         assert self.cal.div(a, b) == expect
