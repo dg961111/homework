f2716d3fc9566cf5c8ae3a1748f5a7ce0839de06
user <275915667@qq.com>
2021-08-09 10:36:24 +0800

    添加allure

--- jenkins0807/selenium_jenkins/test_baidu.py
+++ jenkins0807/selenium_jenkins/test_baidu.py
@@ -1,28 +1,34 @@
 from time import sleep
 
+import allure
 from selenium import webdriver
 
 
+@allure.feature("百度关键词测试")
 class TestBaidu:
 
     def setup(self):
         self.driver = webdriver.Chrome()
         self.driver.get("http://www.baidu.com")
 
+    @allure.story("搜索关键词: sanlu")
     def test_01(self):
         key_word = "sanlu"
         self.search_keyword(key_word)
 
+    @allure.story("搜索关键词: 阿里巴巴")
     def test_02(self):
         key_word = "阿里巴巴"
         self.search_keyword(key_word)
 
     def search_keyword(self, key_word):
-        self.driver.find_element_by_id("kw").send_keys(key_word)
-        self.driver.find_element_by_id("su").click()
+        with allure.step("输入关键字并搜索"):
+            self.driver.find_element_by_id("kw").send_keys(key_word)
+            self.driver.find_element_by_id("su").click()
         sleep(2)
 
-        assert key_word in self.driver.title
+        with allure.step("断言搜索关键字是否存在标题上"):
+            assert key_word in self.driver.title
 
     def teardown(self):
         self.driver.quit()
